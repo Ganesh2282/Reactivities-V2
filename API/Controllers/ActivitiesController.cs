8e4425aa31ca12dacc36feda92013316a6caac53
Ganesh2282 <ganesh.mane@exazeit.com>
2025-05-28 18:17:48 +0530

    test comment"

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,6 +11,7 @@ public class ActivitiesController(AppDbContext context) : BaseApiController
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
+        // test comment
         return await context.Activities.ToListAsync();
     }
 
